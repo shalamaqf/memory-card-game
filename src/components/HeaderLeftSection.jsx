ed64f916565119e8b66f7d476a83d693a9577acd
Shalama <shalamafadillaa@gmail.com>
2026-04-30 06:48:06 +0700

    Create HeaderLeftSection component

---
+++ src/components/HeaderLeftSection.jsx
@@ -0,0 +1,12 @@
+export default function HeaderLeftSection() {
+    return (
+        <div className="header-left-section">
+            <div className="title-container">
+                <h1>Space Objects Memory Card</h1>
+            </div>
+            <p className="game-guide-text">
+                Get points by clicking on an image but don't click on any more than once!
+            </p>
+        </div>
+    )
+}

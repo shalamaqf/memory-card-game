fccf8f83946b70af9f99399ecde351b2ac1e361e
Shalama <shalamafadillaa@gmail.com>
2026-04-30 08:12:54 +0700

    Create CardGrid component

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,17 @@
+import Card from "./Card"
+
+export default function CardGrid({handleClick, SpaceObjects}) {
+    return (
+        <main className="card-grid">
+            {SpaceObjects.map(item => {
+                return <Card 
+                            src={item.imageSrc} 
+                            alt={item.imageAlt} 
+                            spaceObjectName={item.name}
+                            handleClick={handleClick}
+                            key={item.id} 
+                        />
+            })}
+        </main>
+    )
+}

7706e4db7521a222e8694aef5c1c1ec234d00325
Shalama <shalamafadillaa@gmail.com>
2026-04-30 09:16:45 +0700

    Create data array
    Create the array to contains planet's name and its nasa id for fetching to the nasa API.

---
+++ src/data/spaceObjectsData.js
@@ -0,0 +1,15 @@
+const data = [{name: 'sun', nasaId: 'PIA17669'},
+              {name: 'mercury', nasaId: 'PIA11245'},
+              {name: 'venus', nasaId: 'PIA00271'},
+              {name: 'mars', nasaId: 'PIA07893'},
+              {name: 'earth', nasaId: 'PIA00122'},
+              {name: 'moon', nasaId: 'PIA00404'},
+              {name: 'jupiter', nasaId: 'PIA00455'},
+              {name: 'saturn', nasaId: 'PIA05389'},
+              {name: 'uranus', nasaId: 'PIA18182'},
+              {name: 'neptune', nasaId: 'PIA00046'},
+              {name: 'pluto', nasaId: 'PIA19708'},
+              {name: 'titan', nasaId: 'PIA06139'},
+]
+
+export default data

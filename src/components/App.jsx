90d5582241bb792ab5245fd095654269c3c94d85
Shalama <shalamafadillaa@gmail.com>
2026-05-01 19:53:39 +0700

    Create a function to fetch image url from API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,24 @@ export default function App() {
     const [currentScore, setCurrentScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
 
+    // Fetching data images
+    async function fetchData() {
+        const fetchPromises = data.map(item => fetch(`https://images-api.nasa.gov/search?q=${item.nasaId}`));
+        const responses = await Promise.all(fetchPromises);
+        const jsonArray = await Promise.all(responses.map(res => res.json()));
+        const imageUrl = jsonArray.map(item => {
+            if (item.collection.items[0]) {
+                if (item.collection.items[0].links[0]) {
+                    return item.collection.items[0].links[0].href;
+                }
+            }
+
+            return 'https://via.placeholder.com/200?text=No+Image';
+        });
+
+        return imageUrl;
+    }
+
     function handleClick(isClicked) {
         let newScore;
 

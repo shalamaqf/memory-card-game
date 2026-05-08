977bc65b95f6f6bc50f62c27f47feb1ee61611ad
Shalama <shalamafadillaa@gmail.com>
2026-05-08 08:04:32 +0700

    Fix text content

--- src/components/HeaderLeftSection.jsx
+++ src/components/HeaderLeftSection.jsx
@@ -2,7 +2,7 @@ export default function HeaderLeftSection({gameMessage}) {
     return (
         <div className="header-left-section">
             <div className="title-container">
-                <h1>Space Objects Memory Card</h1>
+                <h1>Space Objects Memory Card Game</h1>
             </div>
             <p className="game-guide-text">{gameMessage}</p>
         </div>
--- src/components/HeaderRightSection.jsx
+++ src/components/HeaderRightSection.jsx
@@ -1,7 +1,7 @@
 export default function HeaderRightSection({currentScore, bestScore}) {
     return (
         <div className="score-board-container">
-            <p className="current-score score">Current Score: {currentScore}</p>
+            <p className="current-score score">Score: {currentScore}</p>
             <p className="best-score score">Best Score: {bestScore}</p>
         </div>
     )
